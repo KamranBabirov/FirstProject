d027830198fb3ea3ed4229e6a0bc8a464190a219
Kamran Babirov <kbabiro1@stu.vistula.edu.pl>
2025-02-25 14:35:27 +0100

    Add files via upload

---
+++ Project 1/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int numberToGuess = random.nextInt(100) + 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Guess number between 1 and 100:");
+
+        int guess;
+        do {
+            guess = scanner.nextInt();
+            if (guess < numberToGuess) {
+                System.out.println("Guess higher.");
+            } else if (guess > numberToGuess) {
+                System.out.println("Guess lower.");
+            } else {
+                System.out.println("Congratulations! You've guessed true.");
+            }
+        } while (guess != numberToGuess);
+
+        scanner.close();
+    }
+}
